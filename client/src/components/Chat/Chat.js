f157531e1ec2567cacf1eedfff62c0728c1134d5
OmariBennett <omari.bennett@gmail.com>
2021-09-08 19:22:26 -0400

    Refactor code

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -21,7 +21,7 @@ const Chat = ({ location }) => {
 		socket.emit('join', { name, room }, () => {});
 
 		return () => {
-			// socket.emit('disconnect');
+			socket.emit('disconnect');
 			socket.off();
 		};
 	}, [ENDPOINT, location.search]);
