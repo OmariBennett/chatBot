d02fb4f55639127a461eca556fb16a0d0b6d12dc
OmariBennett <omari.bennett@gmail.com>
2021-09-08 07:50:57 -0400

    Setup Backened server page

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const router = require('./router.js');
 
 const PORT = 5000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+app.use(router); // calling the router as a middleware
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
