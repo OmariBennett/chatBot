5faa76cbced796b02922f54bf8165570efb69150
OmariBennett <omari.bennett@gmail.com>
2021-09-08 08:26:09 -0400

    Refactor Join component & adds Join.css

--- server/index.js
+++ server/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 const router = require('./router.js');
+const { disconnect } = require('process');
 
 const PORT = 5000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+	console.log('We have a new connection.');
+
+	socket.on('disconnect', () => {
+		console.log('User has left the chat!');
+	});
+});
 
 app.use(router); // calling the router as a middleware
 
